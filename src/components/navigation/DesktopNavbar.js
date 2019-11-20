7d54a82b0d80e14181dba509fe5a1eb1ec45722a
Unknown <alonso.m144@gmail.com>
2019-11-20 01:02:16 -0500

    Initial commit

---
+++ src/components/navigation/DesktopNavbar.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const DesktopNavbar = () => {
+  return <div>desktop</div>
+}
+
+export default DesktopNavbar
---
+++ src/components/navigation/MobileNavbar.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const MobileNavbar = () => {
+  return <div>mobile</div>
+}
+
+export default MobileNavbar
